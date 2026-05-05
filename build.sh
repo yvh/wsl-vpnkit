ec55bb244b7569ce481e21ac5384e1949f7072e3
Yannick Vanhaeren <yvh@users.noreply.github.com>
2026-05-05 14:56:41 +0200

    Refactor build.sh for improved argument handling and consistency

--- build.sh
+++ build.sh
@@ -1,21 +1,20 @@
-#!/bin/bash -xe
+#!/usr/bin/env bash
 
-# run from repo root
-# ./build.sh
+set -euox pipefail
 
 : "${DOCKER:=docker}"   # docker/podman command  (default: docker)
 DUMP=wsl-vpnkit.tar.gz  # exported rootfs file
-TAG_NAME=wslvpnkit      # build tag
-#BASE_DISTRO=$1
+TAG_NAME=wsl-vpnkit      # build tag
 
 # build
 build_args=()
-[ -z "${http_proxy}" ] || build_args+=( --build-arg http_proxy="${http_proxy}" )
-[ -z "${https_proxy}" ] || build_args+=( --build-arg https_proxy="${https_proxy}" )
-[ -z "${no_proxy}" ] || build_args+=( --build-arg no_proxy="${no_proxy}" )
-${DOCKER} build --network host "${build_args[@]}" --tag ${TAG_NAME} --file ./distro/.dockerfile .
+for var in http_proxy https_proxy no_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY; do
+    if [ -n "${!var:-}" ]; then
+        build_args+=(--build-arg "$var=${!var}")
+    fi
+done
+${DOCKER} build --network host "${build_args[@]}" --tag ${TAG_NAME} .
 CONTAINER_ID=$(${DOCKER} create ${TAG_NAME})
 ${DOCKER} export "${CONTAINER_ID}" | gzip > ${DUMP}
 ${DOCKER} container rm "${CONTAINER_ID}"
 ls -la ${DUMP}
-
